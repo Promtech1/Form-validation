8cc82db3e7dd358e2be8b4fe07b8a2c59a1c1d6e
Promtech <uwagboepromise5@gmail.com>
2022-05-25 14:44:35 +0100

    Accesed the selectors from the DOM

---
+++ form.js
@@ -0,0 +1,6 @@
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const check = document.querySelector(".error");
+const userList = document.querySelector("#users");
