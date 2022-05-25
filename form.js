8cb1caf7ccf3e3df52a8e3c7af32f78a3994a382
Promtech <uwagboepromise5@gmail.com>
2022-05-25 15:33:58 +0100

    Added validation functionality and added user inputs to a list item and showed them in the DOM

--- form.js
+++ form.js
@@ -4,3 +4,28 @@ const emailInput = document.querySelector("#email");
 const msg = document.querySelector(".msg");
 const check = document.querySelector(".error");
 const userList = document.querySelector("#users");
+
+
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    
+    if(nameInput.value === "" || emailInput.value === ""){
+        msg.classList.add("error");
+        msg.innerHTML = "Please enter all fields";
+        msg.style.display = "block"
+        
+    }else{
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        // clear
+
+        nameInput.value = "";
+        emailInput.value = "";
+    }
+
+    
+}
